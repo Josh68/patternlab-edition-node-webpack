02d1cb11378d04ed7092aeccf450dbb09e0f8687
Josh Schneider <josh68@gmail.com>
2018-04-27 15:56:01 -0700

    more preparation for integrating with v3, cleaning up the core webpack config, pulling in new pl config for file types to watch

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,9 +1,8 @@
 // webpack.config.js
 const webpack = require('webpack');
-const {resolve} = require('path');
+const { resolve } = require('path');
 const globby = require('globby');
-const {getIfUtils, removeEmpty} = require('webpack-config-utils');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
+const { getIfUtils, removeEmpty } = require('webpack-config-utils');
 const EventHooksPlugin = require('event-hooks-webpack-plugin');
 const plConfig = require('./patternlab-config.json');
 const cleanPublic = plConfig.cleanPublic;
@@ -13,19 +12,20 @@ const merge = require('webpack-merge');
 const customization = require(`${plConfig.paths.source.app}/webpack.app.js`);
 
 module.exports = env => {
-  const {ifProd, ifDev} = getIfUtils(env);
-  
+  const { ifProd, ifDev } = getIfUtils(env);
+  const assetsToWatch = plConfig.transformedAssetTypes.join('|');
+
   const config = merge.smartStrategy(plConfig.webpackMerge)({
     devtool: ifDev('source-map'),
     context: resolve(__dirname, 'source'),
     node: {
       fs: "empty"
     },
-    entry: { 
+    entry: {
       // Gathers any Source JS files and creates a bundle
       //NOTE: This name can be changed, if so, make sure to update _meta/01-foot.mustache
-      "js/pl-source": 
-          globby.sync([resolve(plConfig.paths.source.js + '**/*.js')]).map(function (filePath) {
+      "js/pl-source":
+        globby.sync([resolve(plConfig.paths.source.js + '**/*.js')]).map(function (filePath) {
           return filePath;
         })
     },
@@ -33,11 +33,11 @@ module.exports = env => {
       path: resolve(__dirname, 'public'),
       filename: '[name].js'
     },
-    plugins:  removeEmpty([
-      ifDev(
-        // Live reloading in development only
-        new webpack.HotModuleReplacementPlugin()
-      ),
+    plugins: removeEmpty([
+      // ifDev(
+      //   // Live reloading in development only
+      //   new webpack.HotModuleReplacementPlugin()
+      // ),
       ifProd(
         new webpack.optimize.UglifyJsPlugin({
           // Compresses in production any bundle
@@ -55,96 +55,22 @@ module.exports = env => {
           return module.context && /node_modules/.test(module.context);
         }
       }),
-      // All copying and watching now done by core
-      // TBD how this integrates with a webpack pipeline, e.g., for loading SASS, Babel, etc
-      // new CopyWebpackPlugin([
-      //   {
-      //     // Copy all images from source to public
-      //     context: resolve(plConfig.paths.source.images),
-      //     from: './**/*.*',
-      //     to: resolve(plConfig.paths.public.images)
-      //   },
-      //   {
-      //     // Copy favicon from source to public
-      //     context: resolve(plConfig.paths.source.root),
-      //     from: './*.ico',
-      //     to: resolve(plConfig.paths.public.root)
-      //   },
-      //   {
-      //     // Copy all web fonts from source to public
-      //     context: resolve(plConfig.paths.source.fonts),
-      //     from: './*',
-      //     to: resolve(plConfig.paths.public.fonts)
-      //   },
-      //   {
-      //     // Copy all css from source to public
-      //     context: resolve(plConfig.paths.source.css),
-      //     from: './*.css',
-      //     to: resolve(plConfig.paths.public.css)
-      //   },
-      //   {
-      //     // Styleguide Copy everything but css
-      //     context: resolve(plConfig.paths.source.styleguide),
-      //     from: './**/*',
-      //     to: resolve(plConfig.paths.public.root),
-      //     ignore: ['*.css']
-      //   },
-      //   {
-      //     // Styleguide Copy and flatten css
-      //     context: resolve(plConfig.paths.source.styleguide),
-      //     from: './**/*.css',
-      //     to: resolve(plConfig.paths.public.styleguide, 'css'),
-      //     flatten: true
-      //   }
-      // ]),
       new EventHooksPlugin({
-        // After WebPack compiles, call the pattern build API, once done, bundle continues
-        'after-compile': function(compilationParams, callback){
-          //patternlab.build(callback, plConfig.cleanPublic);
-          patternlab.server.reload();
+        'after-compile': function (compilation, callback) {
+          patternlab.buildFrontEnd(
+            {
+              cleanPublic,
+              //watch: bool,
+              //data: object
+            },
+            patternlab
+          )
+
+          // signal done and continue with build
+          callback();
         }
       }),
-      // new EventHooksPlugin({
-      //   'after-compile': function(compilation, callback) {  
-      //     // watch supported templates
-      //     const supportedTemplateExtensions = patternEngines.getSupportedFileExtensions();
-      //     const templateFilePaths = supportedTemplateExtensions.map(function (dotExtension) {
-      //       return plConfig.paths.source.patterns + '/**/*' + dotExtension;
-      //     });
-
-      //     // additional watch files
-      //     const watchFiles = [
-      //       plConfig.paths.source.patterns + '/**/*.json',
-      //       plConfig.paths.source.patterns + '**/*.md',
-      //       plConfig.paths.source.data + '**/*.json',
-      //       plConfig.paths.source.fonts + '**/*',
-      //       plConfig.paths.source.images + '**/*',
-      //       plConfig.paths.source.js + '**/*',
-      //       plConfig.paths.source.meta + '**/*',
-      //       plConfig.paths.source.annotations + '**/*'
-      //     ];
-
-      //     const allWatchFiles = watchFiles.concat(templateFilePaths);
-
-      //     allWatchFiles.forEach(function(globPath) {
-      //       const patternFiles = globby.sync(globPath).map(function (filePath) {
-      //         return resolve(filePath);
-      //       });
-
-      //       compilation.fileDependencies = compilation.fileDependencies.concat(patternFiles);
-      //     });
-
-      //     // signal done and continue with build
-      //     callback();
-      //   }
-      // }),
     ]),
-    devServer: {
-      contentBase: resolve(__dirname, 'public'),
-      port: plConfig.server.port,
-      open:true,
-      watchContentBase: false
-    },
     module: {
       rules: [
         {
@@ -159,6 +85,14 @@ module.exports = env => {
             }
           }]
         }
+      ]
+    },
+    // set up watch with specific options for dev only, in place of devServer, whose duties will now be handled by patternlab-node
+    watch: ifDev(true),
+    watchOptions: {
+      ignored: [
+        /node_modules/,
+        `source/**/*.!(${assetsToWatch})`
       ]
     }
   }, customization(env))
